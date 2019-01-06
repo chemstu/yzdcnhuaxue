0f86847328a84b61ae9f68d045de7a5e00f73100
chemstu <5310449@qq.com>
2019-01-06 18:20:33 +0800

    队列化翻译

--- app/Observers/CategoryObserver.php
+++ app/Observers/CategoryObserver.php
@@ -4,15 +4,18 @@ namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
 use App\Http\Models\Category;
+use App\Jobs\TranslateSlug;
 
 
 class CategoryObserver
 {
-    public function saving(Category $category)
+    public function saved(Category $category)
     {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $category->slug) {
-            $category->slug = app(SlugTranslateHandler::class)->translate($category->title);
+            // 推送任务到队列
+            dispatch(new TranslateSlug($category));
+
         }
     }
 }
